f59b7335d6ec9ea8bcbbadf1eb0c3d0444d1abf3
sumatrabean <kennyho.code@gmail.com>
2024-07-18 15:22:30 -0400

    ability to upload resumes

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useToast } from "@/components/ui/use-toast";
 import { useState } from "react";
+import { createClient } from "@/utils/supabase/client";
 
 function ResumePage() {
   return (
@@ -12,12 +13,13 @@ function ResumePage() {
         <p>Accepted Formats: pdf, docx</p>
         <Uploader />
       </div>
-      <button>upload</button>
     </div>
   );
 }
 
 function Uploader() {
+  const supabase = createClient();
+
   const [data, setData] = useState<{
     image: string | null;
   }>({ image: null });
@@ -26,58 +28,70 @@ function Uploader() {
   const { toast } = useToast();
   console.log(data);
   return (
-    <div
-      className="w-[400px] h-[400px] bg-slate-400 rounded-lg p-4 flex justify-center items-center"
-      onDragOver={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setDragActive(true);
-        console.log("over");
-      }}
-      onDragEnter={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setDragActive(true);
-        console.log("over");
-      }}
-      onDragLeave={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setDragActive(false);
-      }}
-      onDrop={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setDragActive(false);
+    <div>
+      <div
+        className="w-[400px] h-[400px] bg-slate-400 rounded-lg p-4 flex justify-center items-center"
+        onDragOver={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          setDragActive(true);
+          console.log("over");
+        }}
+        onDragEnter={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          setDragActive(true);
+          console.log("over");
+        }}
+        onDragLeave={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          setDragActive(false);
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          setDragActive(false);
 
-        const file = e.dataTransfer.files && e.dataTransfer.files[0];
-        // 250mb
-        const limit = file.size / 1024 / 1024 > 250;
+          const file = e.dataTransfer.files && e.dataTransfer.files[0];
+          // 250mb
+          const limit = file.size / 1024 / 1024 > 250;
 
-        if (limit) {
-          toast({
-            title: "File Not Accepted",
-            description:
-              "Your file is too big! Please upload a file smaller than 250mb",
-          });
-          return;
-        }
+          if (limit) {
+            toast({
+              title: "File Not Accepted",
+              description:
+                "Your file is too big! Please upload a file smaller than 250mb",
+            });
+            return;
+          }
 
-        setFile(file);
-        console.log(file);
-        const reader = new FileReader();
-        console.log(reader);
-        reader.onload = (e) => {
-          setData((prev) => ({
-            ...prev,
-            image: e.target?.result as string,
-          }));
-          console.log("data: ", data);
-        };
-        reader.readAsDataURL(file);
-      }}
-    >
-      uploader
+          setFile(file);
+          console.log(file);
+          const reader = new FileReader();
+          console.log(reader);
+          reader.onload = (e) => {
+            setData((prev) => ({
+              ...prev,
+              image: e.target?.result as string,
+            }));
+            console.log("data: ", data);
+          };
+          reader.readAsDataURL(file);
+        }}
+      ></div>
+      <button
+        onClick={async () => {
+          const supabaseAuth = await supabase.auth.getUser();
+          const userId = supabaseAuth.data.user?.id;
+          const users = await supabase
+            .from("resumes")
+            .upsert({ userId, resumeContent: data });
+          console.log(users);
+        }}
+      >
+        upload
+      </button>
     </div>
   );
 }
